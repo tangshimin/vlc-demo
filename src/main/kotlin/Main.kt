ad5b5e2f651c46cdbac55ad4bf2ce6ca3c6c89ad
shimin tang <tang_shimin@qq.com>
2024-07-18 00:24:58 +0800

    修改 video_url

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,6 @@ import androidx.compose.ui.awt.SwingPanel
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.sun.jna.NativeLibrary
 import kotlinx.coroutines.runBlocking
 import uk.co.caprica.vlcj.factory.discovery.NativeDiscovery
 import uk.co.caprica.vlcj.player.base.MediaPlayer
@@ -16,14 +15,11 @@ import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter
 import uk.co.caprica.vlcj.player.component.CallbackMediaPlayerComponent
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent
 import java.awt.Component
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStreamReader
 import java.util.*
 import kotlin.concurrent.schedule
 import kotlin.system.exitProcess
 
-const val VIDEO_URL = "https://user-images.githubusercontent.com/16540656/174944474-e5947df9-c8ed-4546-9c67-057fe52c2d51.mp4"
+const val VIDEO_URL = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 fun main() = application {
     System.setProperty("compose.interop.blending", "true")
     var isOpen by remember { mutableStateOf(true) }
@@ -95,36 +91,4 @@ fun createMediaPlayerComponent(): Component {
     NativeDiscovery().discover()
     println("开始创建视频播放器...")
     return  CallbackMediaPlayerComponent()
-}
-
-fun checkLibVlcCoreArchitecture() {
-    val libVlcCore = NativeLibrary.getInstance("vlccore")
-    val libVlcCoreFile = libVlcCore.file
-    println("path: ${libVlcCoreFile.absolutePath}")
-    val process = ProcessBuilder("file", libVlcCoreFile.absolutePath).start()
-    val reader = BufferedReader(InputStreamReader(process.inputStream))
-    val output = reader.readLine()
-    println(output)
-}
-fun checkLibraryArchitectures() {
-    val libDirectory = File("/Applications/VLC.app/Contents/MacOS/lib")
-    val libFiles = libDirectory.listFiles { _, name -> name.endsWith(".dylib") }
-
-    libFiles?.forEach { libFile ->
-        val process = ProcessBuilder("file", libFile.absolutePath).start()
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val output = reader.readLine()
-        println("File: ${libFile.name}, Info: $output")
-    }
-}
-fun checkPluginsArchitectures() {
-    val libDirectory = File("/Applications/VLC.app/Contents/MacOS/plugins")
-    val libFiles = libDirectory.listFiles { _, name -> name.endsWith(".dylib") }
-
-    libFiles?.forEach { libFile ->
-        val process = ProcessBuilder("file", libFile.absolutePath).start()
-        val reader = BufferedReader(InputStreamReader(process.inputStream))
-        val output = reader.readLine()
-        println("File: ${libFile.name}, Info: $output")
-    }
 }
